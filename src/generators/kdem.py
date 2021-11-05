805ef6bd821c1e8cf6badbeee11cc08556c0629a
bob <uzebb@student.kit.edu>
2021-11-05 20:03:08 +0100

    Refactor kdem

--- src/generators/kdem.py
+++ src/generators/kdem.py
@@ -1,31 +1,30 @@
-
 import sys
-import warnings
 import numpy as np
 from sklearn.neighbors import KernelDensity
 from statsmodels.nonparametric.bandwidths import bw_silverman, bw_scott
-
 # to choose bandwidth via CV, see, for instance, 
 # https://scikit-learn.org/stable/auto_examples/neighbors/plot_digits_kde_sampling.html#sphx-glr-auto-examples-neighbors-plot-digits-kde-sampling-py
 
-class Gen_kdebwm:          
 
-    def __init__(self, method = 'silverman'):
+class Gen_kdebwm:
+
+    def __init__(self, method='silverman'):
         if method == 'silverman':
             self.bw_method_ = bw_silverman
         elif method == 'scott':
             self.bw_method_ = bw_scott
         else:
-            sys.exit("The method must be either scott or silverman")
+            raise ValueError("The method must be either scott or silverman")
+        self.model_ = None
 
-    def fit(self, X, y = None):
+    def fit(self, X, y=None, metamodel=None):
         bw = self.bw_method_(X)
         self.model_ = []
         for i in np.arange(X.shape[1]):
-            self.model_.append(KernelDensity(bandwidth = bw[i]).fit(X[:,i].reshape(-1, 1)))
+            self.model_.append(KernelDensity(bandwidth=bw[i]).fit(X[:,i].reshape(-1, 1)))
         return self
 
-    def sample(self, n_samples = 1):
+    def sample(self, n_samples=1):
         newdata = []
         for i in np.arange(len(self.model_)):
             newdata.append(self.model_[i].sample(n_samples))
