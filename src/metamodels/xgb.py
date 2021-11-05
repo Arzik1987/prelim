c1be6bb6deb08919016f9ab6e0cd5fcbd76757aa
bob <uzebb@student.kit.edu>
2021-11-05 20:41:28 +0100

    Refactor xgb

--- src/metamodels/xgb.py
+++ src/metamodels/xgb.py
@@ -5,20 +5,25 @@ from scipy.stats import uniform, randint
 
     
 class Meta_xgb:
-    def __init__(self, params = {'n_estimators' : randint(10,990),
-        'learning_rate' : uniform(0.0001,0.2),
-        'gamma' : uniform(0,0.4),
-        'max_depth' : [6],
-        'subsample' : uniform(0.5,0.5)}, cv = 5, seed = 2020):
-        
+    def __init__(self, params=None, cv=5, seed=2020):
+        if params is None:
+            params = {
+                'n_estimators': randint(10, 990),
+                'learning_rate': uniform(0.0001, 0.2),
+                'gamma': uniform(0, 0.4),
+                'max_depth': [6],
+                'subsample': uniform(0.5, 0.5)
+            }
         self.params_ = params
         self.cv_ = cv
         self.seed_ = seed
+        self.model_ = None
+        self.cvscore_ = None
 
     def fit(self, X, y):
-        tmp = RandomizedSearchCV(XGBClassifier(nthread = 1, verbosity = 0, use_label_encoder = False), 
-                                         self.params_, random_state = self.seed_,
-                                         cv = self.cv_, n_iter = 50, n_jobs = 1).fit(X, y)
+        tmp = RandomizedSearchCV(XGBClassifier(nthread=1, verbosity=0, use_label_encoder=False), self.params_,
+                                 random_state=self.seed_, cv=self.cv_, n_iter=50, n_jobs=1)
+        tmp.fit(X, y)
         self.model_ = tmp.best_estimator_
         self.cvscore_ = tmp.best_score_
         return self
