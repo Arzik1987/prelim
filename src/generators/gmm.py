7c7fc33afd5ffd89dd1348da1b068bba1573eb78
bob <uzebb@student.kit.edu>
2021-11-05 19:53:48 +0100

    Refactor gmm

--- src/generators/gmm.py
+++ src/generators/gmm.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from sklearn.mixture import GaussianMixture
 from sklearn.model_selection import GridSearchCV
@@ -6,29 +5,42 @@ from sklearn.model_selection import GridSearchCV
 
 class Gen_gmm:
     
-    def __init__(self, params = {"n_components": list(range(1, 30)), 
-                                 "covariance_type": ["full", "tied", "diag", "spherical"]}, cv = 5):
-        self.params_ = params
+    def __init__(self, params: dict = None, cv=5):
+        if params is None:
+            self.params_ = {
+                "covariance_type": ["full", "tied", "diag", "spherical"],
+                "n_components": list(range(1,31))
+            }
+        else:
+            self.params_ = params
+
+        self.model_ = None
         self.cv_ = cv
 
-    def fit(self, X):
+    def fit(self, X, y=None, metamodel=None):
         self.model_ = GridSearchCV(GaussianMixture(), self.params_, cv = self.cv_).fit(X).best_estimator_
         return self
 
-    def sample(self, n_samples = 1, X_new = None):
+    def sample(self, n_samples=1, X_new=None):
         return self.model_.sample(n_samples)[0]
     
     def my_name(self):
         return "gmmcv"
-    
-    
+
+
 class Gen_gmmbic:
 
-    def __init__(self, params = {"n_components": list(range(1, 30)), 
-                                 "covariance_type": ["full", "tied", "diag", "spherical"]}, cv = 5):
-        self.params_ = params
+    def __init__(self, params: dict = None, cv=None):
+        if params is None:
+            self.params_ = {
+                "covariance_type": ["full", "tied", "diag", "spherical"],
+                "n_components": list(range(1,31))
+            }
+        else:
+            self.params_ = params
+        self.model_ = None
 
-    def fit(self, X, y = None):
+    def fit(self, X, y=None, metamodel=None):
         # see https://scikit-learn.org/stable/auto_examples/mixture/plot_gmm_selection.html
         lowest_bic = np.infty
         for cv_type in self.params_['covariance_type']:
@@ -52,10 +64,13 @@ class Gen_gmmbic:
 
 class Gen_gmmbical:
 
-    def __init__(self, params = {"n_components": list(range(1, 30))}, cv = 5):
-        self.params_ = params
+    def __init__(self, params: dict = None, cv=None):
+        if params is None:
+            self.params_ = {"n_components": list(range(1,31))}
+        else:
+            self.params_ = params
 
-    def fit(self, X, y = None):
+    def fit(self, X, y=None, metamodel=None):
         # see https://scikit-learn.org/stable/auto_examples/mixture/plot_gmm_selection.html
         lowest_bic = np.infty
         for n_components in self.params_['n_components']:
@@ -77,8 +92,8 @@ class Gen_gmmbical:
 
 
 # =============================================================================
-# # TEST 
-# 
+# TEST
+
 # mean = [0, 0]
 # cov = [[1, 0], [0, 1]]
 # x = np.random.multivariate_normal(mean, cov, 500)
@@ -86,16 +101,25 @@ class Gen_gmmbical:
 # x = np.vstack((x,np.random.multivariate_normal(mean, cov, 500)))
 # import matplotlib.pyplot as plt
 # plt.scatter(x[:,0], x[:,1])
-# 
+# plt.show()
+#
 # gmm = Gen_gmm()
 # gmm.fit(x)
 # df = gmm.sample(n_samples = 201)
 # plt.scatter(df[:,0], df[:,1])
-# 
+# plt.show()
+#
 # gmm = Gen_gmmbic()
 # gmm.fit(x)
 # df = gmm.sample(n_samples = 201)
 # plt.scatter(df[:,0], df[:,1])
+# plt.show()
+#
+# gmm = Gen_gmmbical()
+# gmm.fit(x)
+# df = gmm.sample(n_samples = 201)
+# plt.scatter(df[:,0], df[:,1])
+# plt.show()
 # =============================================================================
 
 
